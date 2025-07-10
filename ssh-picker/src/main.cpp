93aec5199af86fc98f59a28acaf75c4c89b95734
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-10 21:39:40 +0300

    #5: Format `main.cpp`

--- ssh-picker/src/main.cpp
+++ ssh-picker/src/main.cpp
@@ -1,29 +1,25 @@
 #include <functional>
-#include <iostream>
 #include <string>
 #include <vector>
 
-#include "ftxui/component/captured_mouse.hpp"      // for ftxui
 #include "ftxui/component/component.hpp"           // for Menu
 #include "ftxui/component/component_options.hpp"   // for MenuOption
 #include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
 
 int main() {
-  using namespace ftxui;
-  auto screen = ScreenInteractive::TerminalOutput();
+    using namespace ftxui;
+    auto screen = ScreenInteractive::TerminalOutput();
 
-  std::vector<std::string> entries = {
-      "entry 1",
-      "entry 2",
-      "entry 3",
-  };
-  int selected = 0;
+    std::vector<std::string> entries = {
+        "entry 1",
+        "entry 2",
+        "entry 3",
+    };
+    int selected = 0;
 
-  MenuOption option;
-  option.on_enter = screen.ExitLoopClosure();
-  auto menu = Menu(&entries, &selected, option);
+    MenuOption option;
+    option.on_enter = screen.ExitLoopClosure();
+    auto menu = Menu(&entries, &selected, option);
 
-  screen.Loop(menu);
-
-  std::cout << "Selected element = " << selected << std::endl;
+    screen.Loop(menu);
 }
